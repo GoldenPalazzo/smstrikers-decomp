df63cdb6131acca9aee111a10be6f78a24f5ad2b
Yannick Suter <yannick.suter@gmail.com>
2026-03-25 09:38:24 +0100

    fixing clang format issues

--- src/Dolphin/mtx/mtx44.c
+++ src/Dolphin/mtx/mtx44.c
@@ -121,6 +121,7 @@ void PSMTX44Identity(register Mtx44 m)
     register f32 c1 = 1.0f;
     register f32 c0 = 0.0f;
 
+    // clang-format off
     asm {
         stfs c1, 0x0(m)
         psq_st c0, 0x4(m), 0, 0
@@ -133,6 +134,7 @@ void PSMTX44Identity(register Mtx44 m)
         psq_st c0, 0x34(m), 0, 0
         stfs c1, 0x3c(m)
     }
+    // clang-format on
 }
 
 void C_MTX44Copy(const Mtx44 src, Mtx44 dst)
@@ -161,12 +163,29 @@ void C_MTX44Copy(const Mtx44 src, Mtx44 dst)
     }
 }
 
+// clang-format off
 asm void PSMTX44Copy(const register Mtx44 src, register Mtx44 dst)
 {
     nofralloc
-        psq_l f1,
-        0x0(src), 0, 0 psq_st f1, 0x0(dst), 0, 0 psq_l f1, 0x8(src), 0, 0 psq_st f1, 0x8(dst), 0, 0 psq_l f1, 0x10(src), 0, 0 psq_st f1, 0x10(dst), 0, 0 psq_l f1, 0x18(src), 0, 0 psq_st f1, 0x18(dst), 0, 0 psq_l f1, 0x20(src), 0, 0 psq_st f1, 0x20(dst), 0, 0 psq_l f1, 0x28(src), 0, 0 psq_st f1, 0x28(dst), 0, 0 psq_l f1, 0x30(src), 0, 0 psq_st f1, 0x30(dst), 0, 0 psq_l f1, 0x38(src), 0, 0 psq_st f1, 0x38(dst), 0, 0 blr
+    psq_l f1, 0x0(src), 0, 0
+    psq_st f1, 0x0(dst), 0, 0
+    psq_l f1, 0x8(src), 0, 0
+    psq_st f1, 0x8(dst), 0, 0
+    psq_l f1, 0x10(src), 0, 0
+    psq_st f1, 0x10(dst), 0, 0
+    psq_l f1, 0x18(src), 0, 0
+    psq_st f1, 0x18(dst), 0, 0
+    psq_l f1, 0x20(src), 0, 0
+    psq_st f1, 0x20(dst), 0, 0
+    psq_l f1, 0x28(src), 0, 0
+    psq_st f1, 0x28(dst), 0, 0
+    psq_l f1, 0x30(src), 0, 0
+    psq_st f1, 0x30(dst), 0, 0
+    psq_l f1, 0x38(src), 0, 0
+    psq_st f1, 0x38(dst), 0, 0
+    blr
 }
+// clang-format on
 
 void C_MTX44Concat(const Mtx44 a, const Mtx44 b, Mtx44 ab)
 {
@@ -212,12 +231,77 @@ void C_MTX44Concat(const Mtx44 a, const Mtx44 b, Mtx44 ab)
     }
 }
 
+// clang-format off
 asm void PSMTX44Concat(const register Mtx44 a, const register Mtx44 b, register Mtx44 ab)
 {
     nofralloc
-        psq_l f0,
-        0x0(a), 0, 0 psq_l f2, 0x0(b), 0, 0 ps_muls0 f6, f2, f0 psq_l f3, 0x10(b), 0, 0 psq_l f4, 0x20(b), 0, 0 ps_madds1 f6, f3, f0, f6 psq_l f1, 0x8(a), 0, 0 psq_l f5, 0x30(b), 0, 0 ps_madds0 f6, f4, f1, f6 psq_l f0, 0x10(a), 0, 0 ps_madds1 f6, f5, f1, f6 psq_l f1, 0x18(a), 0, 0 ps_muls0 f8, f2, f0 ps_madds1 f8, f3, f0, f8 psq_l f0, 0x20(a), 0, 0 ps_madds0 f8, f4, f1, f8 ps_madds1 f8, f5, f1, f8 psq_l f1, 0x28(a), 0, 0 ps_muls0 f10, f2, f0 ps_madds1 f10, f3, f0, f10 psq_l f0, 0x30(a), 0, 0 ps_madds0 f10, f4, f1, f10 ps_madds1 f10, f5, f1, f10 psq_l f1, 0x38(a), 0, 0 ps_muls0 f12, f2, f0 psq_l f2, 0x8(b), 0, 0 ps_madds1 f12, f3, f0, f12 psq_l f0, 0x0(a), 0, 0 ps_madds0 f12, f4, f1, f12 psq_l f3, 0x18(b), 0, 0 ps_madds1 f12, f5, f1, f12 psq_l f1, 0x8(a), 0, 0 ps_muls0 f7, f2, f0 psq_l f4, 0x28(b), 0, 0 ps_madds1 f7, f3, f0, f7 psq_l f5, 0x38(b), 0, 0 ps_madds0 f7, f4, f1, f7 psq_l f0, 0x10(a), 0, 0 ps_madds1 f7, f5, f1, f7 psq_l f1, 0x18(a), 0, 0 ps_muls0 f9, f2, f0 psq_st f6, 0x0(ab), 0, 0 ps_madds1 f9, f3, f0, f9 psq_l f0, 0x20(a), 0, 0 ps_madds0 f9, f4, f1, f9 psq_st f8, 0x10(ab), 0, 0 ps_madds1 f9, f5, f1, f9 psq_l f1, 0x28(a), 0, 0 ps_muls0 f11, f2, f0 psq_st f10, 0x20(ab), 0, 0 ps_madds1 f11, f3, f0, f11 psq_l f0, 0x30(a), 0, 0 ps_madds0 f11, f4, f1, f11 psq_st f12, 0x30(ab), 0, 0 ps_madds1 f11, f5, f1, f11 psq_l f1, 0x38(a), 0, 0 ps_muls0 f13, f2, f0 psq_st f7, 0x8(ab), 0, 0 ps_madds1 f13, f3, f0, f13 psq_st f9, 0x18(ab), 0, 0 ps_madds0 f13, f4, f1, f13 psq_st f11, 0x28(ab), 0, 0 ps_madds1 f13, f5, f1, f13 psq_st f13, 0x38(ab), 0, 0 blr
+    psq_l f0, 0x0(a), 0, 0
+    psq_l f2, 0x0(b), 0, 0
+    ps_muls0 f6, f2, f0
+    psq_l f3, 0x10(b), 0, 0
+    psq_l f4, 0x20(b), 0, 0
+    ps_madds1 f6, f3, f0, f6
+    psq_l f1, 0x8(a), 0, 0
+    psq_l f5, 0x30(b), 0, 0
+    ps_madds0 f6, f4, f1, f6
+    psq_l f0, 0x10(a), 0, 0
+    ps_madds1 f6, f5, f1, f6
+    psq_l f1, 0x18(a), 0, 0
+    ps_muls0 f8, f2, f0
+    ps_madds1 f8, f3, f0, f8
+    psq_l f0, 0x20(a), 0, 0
+    ps_madds0 f8, f4, f1, f8
+    ps_madds1 f8, f5, f1, f8
+    psq_l f1, 0x28(a), 0, 0
+    ps_muls0 f10, f2, f0
+    ps_madds1 f10, f3, f0, f10
+    psq_l f0, 0x30(a), 0, 0
+    ps_madds0 f10, f4, f1, f10
+    ps_madds1 f10, f5, f1, f10
+    psq_l f1, 0x38(a), 0, 0
+    ps_muls0 f12, f2, f0
+    psq_l f2, 0x8(b), 0, 0
+    ps_madds1 f12, f3, f0, f12
+    psq_l f0, 0x0(a), 0, 0
+    ps_madds0 f12, f4, f1, f12
+    psq_l f3, 0x18(b), 0, 0
+    ps_madds1 f12, f5, f1, f12
+    psq_l f1, 0x8(a), 0, 0
+    ps_muls0 f7, f2, f0
+    psq_l f4, 0x28(b), 0, 0
+    ps_madds1 f7, f3, f0, f7
+    psq_l f5, 0x38(b), 0, 0
+    ps_madds0 f7, f4, f1, f7
+    psq_l f0, 0x10(a), 0, 0
+    ps_madds1 f7, f5, f1, f7
+    psq_l f1, 0x18(a), 0, 0
+    ps_muls0 f9, f2, f0
+    psq_st f6, 0x0(ab), 0, 0
+    ps_madds1 f9, f3, f0, f9
+    psq_l f0, 0x20(a), 0, 0
+    ps_madds0 f9, f4, f1, f9
+    psq_st f8, 0x10(ab), 0, 0
+    ps_madds1 f9, f5, f1, f9
+    psq_l f1, 0x28(a), 0, 0
+    ps_muls0 f11, f2, f0
+    psq_st f10, 0x20(ab), 0, 0
+    ps_madds1 f11, f3, f0, f11
+    psq_l f0, 0x30(a), 0, 0
+    ps_madds0 f11, f4, f1, f11
+    psq_st f12, 0x30(ab), 0, 0
+    ps_madds1 f11, f5, f1, f11
+    psq_l f1, 0x38(a), 0, 0
+    ps_muls0 f13, f2, f0
+    psq_st f7, 0x8(ab), 0, 0
+    ps_madds1 f13, f3, f0, f13
+    psq_st f9, 0x18(ab), 0, 0
+    ps_madds0 f13, f4, f1, f13
+    psq_st f11, 0x28(ab), 0, 0
+    ps_madds1 f13, f5, f1, f13
+    psq_st f13, 0x38(ab), 0, 0
+    blr
 }
+// clang-format on
 
 void C_MTX44Transpose(const Mtx44 src, Mtx44 xPose)
 {
@@ -259,82 +343,36 @@ void C_MTX44Transpose(const Mtx44 src, Mtx44 xPose)
     }
 }
 
+// clang-format off
 asm void PSMTX44Transpose(const register Mtx44 src, register Mtx44 xPose) {
     nofralloc
-        psq_l f0,
-    0x0(src),
-    0,
-    0 psq_l f1,
-    0x10(src),
-    0,
-    0 ps_merge00 f4,
-    f0,
-    f1
-        psq_l f2,
-    0x8(src),
-    0,
-    0 psq_st f4,
-    0x0(xPose),
-    0,
-    0 ps_merge11 f5,
-    f0,
-    f1
-        psq_l f3,
-    0x18(src),
-    0,
-    0 psq_st f5,
-    0x10(xPose),
-    0,
-    0 ps_merge00 f4,
-    f2,
-    f3
-        psq_l f0,
-    0x20(src),
-    0,
-    0 psq_st f4,
-    0x20(xPose),
-    0,
-    0 ps_merge11 f5,
-    f2,
-    f3
-        psq_l f1,
-    0x30(src),
-    0,
-    0 psq_st f5,
-    0x30(xPose),
-    0,
-    0 ps_merge00 f4,
-    f0,
-    f1
-        psq_l f2,
-    0x28(src),
-    0,
-    0 psq_st f4,
-    0x8(xPose),
-    0,
-    0 ps_merge11 f5,
-    f0,
-    f1
-        psq_l f3,
-    0x38(src),
-    0,
-    0 psq_st f5,
-    0x18(xPose),
-    0,
-    0 ps_merge00 f4,
-    f2,
-    f3
-        psq_st f4,
-    0x28(xPose),
-    0,
-    0 ps_merge11 f5,
-    f2,
-    f3
-        psq_st f5,
-    0x38(xPose),
-    0,
-    0 blr
+    psq_l f0, 0x0(src), 0, 0
+    psq_l f1, 0x10(src), 0, 0
+    ps_merge00 f4, f0, f1
+    psq_l f2, 0x8(src), 0, 0
+    psq_st f4, 0x0(xPose), 0, 0
+    ps_merge11 f5, f0, f1
+    psq_l f3, 0x18(src), 0, 0
+    psq_st f5, 0x10(xPose), 0, 0
+    ps_merge00 f4, f2, f3
+    psq_l f0, 0x20(src), 0, 0
+    psq_st f4, 0x20(xPose), 0, 0
+    ps_merge11 f5, f2, f3
+    psq_l f1, 0x30(src), 0, 0
+    psq_st f5, 0x30(xPose), 0, 0
+    ps_merge00 f4, f0, f1
+    psq_l f2, 0x28(src), 0, 0
+    psq_st f4, 0x8(xPose), 0, 0
+    ps_merge11 f5, f0, f1
+    psq_l f3, 0x38(src), 0, 0
+    psq_st f5, 0x18(xPose), 0, 0
+    ps_merge00 f4, f2, f3
+    psq_st f4, 0x28(xPose), 0, 0
+    ps_merge11 f5, f2, f3
+    psq_st f5, 0x38(xPose), 0, 0
+    blr
 }
+// clang-format on
 
 #define SWAP(a, b) \
     {              \
@@ -368,7 +406,7 @@ u32 C_MTX44Inverse(const Mtx44 src, Mtx44 inv)
 
         for (k = i; k < 4; k++)
         {
-            ftmp = __fabsf(gjm[k][i]);
+            ftmp = fabsf(gjm[k][i]);
             if (ftmp > max)
             {
                 max = ftmp;
@@ -445,6 +483,7 @@ void PSMTX44Trans(register Mtx44 m, register f32 xT, register f32 yT, register f
     register f32 c_one = 1.0f;
     register f32 c_01;
 
+    // clang-format off
     asm {
         stfs xT, 0xc(m)
         stfs yT, 0x1c(m)
@@ -459,6 +498,7 @@ void PSMTX44Trans(register Mtx44 m, register f32 xT, register f32 yT, register f
         psq_st c_zero, 0x30(m), 0, 0
         psq_st c_01, 0x38(m), 0, 0
     }
+    // clang-format on
 }
 
 void C_MTX44TransApply(const Mtx44 src, Mtx44 dst, f32 xT, f32 yT, f32 zT)
@@ -492,12 +532,35 @@ void C_MTX44TransApply(const Mtx44 src, Mtx44 dst, f32 xT, f32 yT, f32 zT)
     dst[2][3] = (src[2][3] + zT);
 }
 
+// clang-format off
 asm void PSMTX44TransApply(const register Mtx44 src, register Mtx44 dst, register f32 xT, register f32 yT, register f32 zT)
 {
     nofralloc
-        psq_l f4,
-        0x0(src), 0, 0 frsp xT, xT psq_l f5, 0x8(src), 0, 0 frsp yT, yT psq_l f6, 0x10(src), 0, 0 frsp zT, zT psq_l f7, 0x18(src), 0, 0 psq_st f4, 0x0(dst), 0, 0 ps_sum1 f5, xT, f5, f5 psq_l f4, 0x28(src), 0, 0 psq_st f6, 0x10(dst), 0, 0 ps_sum1 f7, yT, f7, f7 psq_l f8, 0x20(src), 0, 0 psq_st f5, 0x8(dst), 0, 0 ps_sum1 f4, zT, f4, f4 psq_st f7, 0x18(dst), 0, 0 psq_st f8, 0x20(dst), 0, 0 psq_l f5, 0x30(src), 0, 0 psq_l f6, 0x38(src), 0, 0 psq_st f4, 0x28(dst), 0, 0 psq_st f5, 0x30(dst), 0, 0 psq_st f6, 0x38(dst), 0, 0 blr
+    psq_l f4, 0x0(src), 0, 0
+    frsp xT, xT
+    psq_l f5, 0x8(src), 0, 0
+    frsp yT, yT
+    psq_l f6, 0x10(src), 0, 0
+    frsp zT, zT
+    psq_l f7, 0x18(src), 0, 0
+    psq_st f4, 0x0(dst), 0, 0
+    ps_sum1 f5, xT, f5, f5
+    psq_l f4, 0x28(src), 0, 0
+    psq_st f6, 0x10(dst), 0, 0
+    ps_sum1 f7, yT, f7, f7
+    psq_l f8, 0x20(src), 0, 0
+    psq_st f5, 0x8(dst), 0, 0
+    ps_sum1 f4, zT, f4, f4
+    psq_st f7, 0x18(dst), 0, 0
+    psq_st f8, 0x20(dst), 0, 0
+    psq_l f5, 0x30(src), 0, 0
+    psq_l f6, 0x38(src), 0, 0
+    psq_st f4, 0x28(dst), 0, 0
+    psq_st f5, 0x30(dst), 0, 0
+    psq_st f6, 0x38(dst), 0, 0
+    blr
 }
+// clang-format on
 
 void C_MTX44Scale(Mtx44 m, f32 xS, f32 yS, f32 zS)
 {
@@ -528,6 +591,7 @@ void PSMTX44Scale(register Mtx44 m, register f32 xS, register f32 yS, register f
     register f32 c_zero = 0.0f;
     register f32 c_one = 1.0f;
 
+    // clang-format off
     asm {
         stfs xS, 0x0(m)
         psq_st c_zero, 0x4(m), 0, 0
@@ -540,6 +604,7 @@ void PSMTX44Scale(register Mtx44 m, register f32 xS, register f32 yS, register f
         psq_st c_zero, 0x34(m), 0, 0
         stfs c_one, 0x3c(m)
     }
+    // clang-format on
 }
 
 void C_MTX44ScaleApply(const Mtx44 src, Mtx44 dst, f32 xS, f32 yS, f32 zS)
@@ -568,12 +633,38 @@ void C_MTX44ScaleApply(const Mtx44 src, Mtx44 dst, f32 xS, f32 yS, f32 zS)
     dst[3][3] = src[3][3];
 }
 
+// clang-format off
 asm void PSMTX44ScaleApply(const register Mtx44 src, register Mtx44 dst, register f32 xS, register f32 yS, register f32 zS)
 {
     nofralloc
-        psq_l f4,
-        0x0(src), 0, 0 frsp xS, xS psq_l f5, 0x8(src), 0, 0 frsp yS, yS psq_l f6, 0x10(src), 0, 0 ps_muls0 f4, f4, xS psq_l f7, 0x18(src), 0, 0 ps_muls0 f5, f5, xS psq_l f8, 0x20(src), 0, 0 frsp zS, zS psq_st f4, 0x0(dst), 0, 0 ps_muls0 f6, f6, yS psq_l f9, 0x28(src), 0, 0 psq_st f5, 0x8(dst), 0, 0 ps_muls0 f7, f7, yS psq_l f10, 0x30(src), 0, 0 psq_st f6, 0x10(dst), 0, 0 ps_muls0 f8, f8, zS psq_l f11, 0x38(src), 0, 0 psq_st f7, 0x18(dst), 0, 0 ps_muls0 f9, f9, zS psq_st f8, 0x20(dst), 0, 0 psq_st f9, 0x28(dst), 0, 0 psq_st f10, 0x30(dst), 0, 0 psq_st f11, 0x38(dst), 0, 0 blr
+    psq_l f4, 0x0(src), 0, 0
+    frsp xS, xS
+    psq_l f5, 0x8(src), 0, 0
+    frsp yS, yS
+    psq_l f6, 0x10(src), 0, 0
+    ps_muls0 f4, f4, xS
+    psq_l f7, 0x18(src), 0, 0
+    ps_muls0 f5, f5, xS
+    psq_l f8, 0x20(src), 0, 0
+    frsp zS, zS
+    psq_st f4, 0x0(dst), 0, 0
+    ps_muls0 f6, f6, yS
+    psq_l f9, 0x28(src), 0, 0
+    psq_st f5, 0x8(dst), 0, 0
+    ps_muls0 f7, f7, yS
+    psq_l f10, 0x30(src), 0, 0
+    psq_st f6, 0x10(dst), 0, 0
+    ps_muls0 f8, f8, zS
+    psq_l f11, 0x38(src), 0, 0
+    psq_st f7, 0x18(dst), 0, 0
+    ps_muls0 f9, f9, zS
+    psq_st f8, 0x20(dst), 0, 0
+    psq_st f9, 0x28(dst), 0, 0
+    psq_st f10, 0x30(dst), 0, 0
+    psq_st f11, 0x38(dst), 0, 0
+    blr
 }
+// clang-format on
 
 void C_MTX44RotRad(Mtx44 m, char axis, f32 rad)
 {
@@ -674,6 +765,7 @@ void PSMTX44RotTrig(register Mtx44 m, register char axis, register f32 sinA, reg
     register f32 c_zero = 0.0f;
     register f32 c_one = 1.0f;
 
+    // clang-format off
     asm {
         frsp sinA, sinA
         ori axis, axis, 0x20
@@ -730,6 +822,7 @@ void PSMTX44RotTrig(register Mtx44 m, register char axis, register f32 sinA, reg
         psq_st ftmp4, 0x0(m), 0, 0
     L_00001B54:
     }
+    // clang-format on
 }
 
 void C_MTX44RotAxisRad(Mtx44 m, const Vec* axis, f32 rad)
@@ -801,6 +894,7 @@ static void __PSMTX44RotAxisRadInternal(register Mtx44 m, const register Vec* ax
     tmp9 = 0.5f;
     tmp8 = 3.0f;
 
+    // clang-format off
     asm {
         frsp cT, cT
         psq_l tmp0, 0x0(axis), 0, 0
@@ -847,6 +941,7 @@ static void __PSMTX44RotAxisRadInternal(register Mtx44 m, const register Vec* ax
         psq_st tmp4, 0x20(m), 0, 0
         psq_st tmp5, 0x28(m), 0, 0
     }
+    // clang-format on
 }
 
 void PSMTX44RotAxisRad(Mtx44 m, const Vec* axis, f32 rad)
