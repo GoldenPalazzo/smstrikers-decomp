d8f2c758aa078a18fe3c86ec714369583752830c
Yannick Suter <yannick.suter@gmail.com>
2026-07-25 22:47:22 +0200

    matched ShapeRender::CreateCylinderGeometry

--- src/Game/Debug/ShapeRender.cpp
+++ src/Game/Debug/ShapeRender.cpp
@@ -244,7 +244,6 @@ void ShapeRender::CreateFlatCylinderEndGeometry(PrimitiveShape& prim)
 
 /**
  * Offset/Address/Size: 0xE14 | 0x801FC0A4 | size: 0x3AC
- * TODO: 99.30% match - remaining saved-FPR allocation for constants differs.
  */
 void ShapeRender::CreateCylinderGeometry(PrimitiveShape& prim)
 {
@@ -261,10 +260,6 @@ void ShapeRender::CreateCylinderGeometry(PrimitiveShape& prim)
     float y0;
     float x1;
     float y1;
-    float x0Sq;
-    float y0Sq;
-    float x1Sq;
-    float y1Sq;
     float invLen;
 
     prim.vertCount = 0x40;
@@ -278,11 +273,13 @@ void ShapeRender::CreateCylinderGeometry(PrimitiveShape& prim)
 
     for (nRing = 0; nRing < 2; nRing++)
     {
-        z0 = -0.5f + (float)nRing * 0.5f;
-        z1 = -0.5f + (float)(nRing + 1) * 0.5f;
+        float ringScale = 0.5f;
 
-        nlSin((u16)((u16)(int)(10430.378f * ((float)nRing * 0.5f)) + 0x4000));
-        nlSin((u16)((u16)(int)(10430.378f * ((float)(nRing + 1) * 0.5f)) + 0x4000));
+        z0 = -0.5f + (float)nRing * ringScale;
+        z1 = -0.5f + (float)(nRing + 1) * ringScale;
+
+        nlSin((u16)((u16)(int)(10430.378f * ((float)nRing * ringScale)) + 0x4000));
+        nlSin((u16)((u16)(int)(10430.378f * ((float)(nRing + 1) * ringScale)) + 0x4000));
 
         for (int nSegment = 0; nSegment < 0x10; nSegment++)
         {
@@ -299,14 +296,11 @@ void ShapeRender::CreateCylinderGeometry(PrimitiveShape& prim)
             x1 = 0.5f * nlSin((u16)angle);
             y1 = 0.5f * nlSin((u16)angle90);
 
-            x0Sq = x0 * x0;
-            y0Sq = y0 * y0;
-
             vNormal.f.x = x0;
             vNormal.f.y = y0;
             vNormal.f.z = z0;
 
-            invLen = nlRecipSqrt((z0 * z0) + (x0Sq + y0Sq), true);
+            invLen = nlRecipSqrt(nlVec3LengthSquared(vNormal), true);
 
             pdst->f.x = x0;
             nlVec3Scale(vNormal, invLen);
@@ -317,14 +311,11 @@ void ShapeRender::CreateCylinderGeometry(PrimitiveShape& prim)
             tdst->f.x = (float)nSegment / 15.0f;
             tdst->f.y = (float)nRing / 2.0f;
 
-            x1Sq = x1 * x1;
-            y1Sq = y1 * y1;
-
             vNormal.f.x = x1;
             vNormal.f.y = y1;
             vNormal.f.z = z1;
 
-            invLen = nlRecipSqrt((z1 * z1) + (x1Sq + y1Sq), true);
+            invLen = nlRecipSqrt(nlVec3LengthSquared(vNormal), true);
 
             pdst[1].f.x = x1;
             nlVec3Scale(vNormal, invLen);
