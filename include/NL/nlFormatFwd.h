0518ee17db0ea84b19cc617ff17d81b3a92e25e6
Francesco Palazzo <francesco@palazzo.me>
2026-09-06 15:48:19 +0200

    experimental patch: (basename ../../patches/tmp/003-remove-format-fwddecl.patch .patch)

--- include/NL/nlFormatFwd.h
+++ include/NL/nlFormatFwd.h
@@ -1,7 +1,9 @@
 #ifndef _NLFORMATFWD_H_
 #define _NLFORMATFWD_H_
 
-template <typename StringType, typename T0>
-StringType Format(const StringType& string, const T0& t0);
+#include "NL/nlFormat.h"
+
+// template <typename StringType, typename T0>
+// StringType Format(const StringType& string, const T0& t0);
 
 #endif // _NLFORMATFWD_H_
